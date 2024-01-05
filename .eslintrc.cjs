37655a3c932bc635284f260cf5adc55f1efa1108
fdemian <fdemian@gmail.com>
2024-01-04 23:03:05 -0300

    Fix build on os x.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,11 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
